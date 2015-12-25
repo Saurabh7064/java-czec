3306ad4fa4edeff38e3387149e48dbc317fee2fb
surbhi <surbhi@gmail.com>
2015-12-26 01:08:00 +0530

    bootstrap modal and tab added to add and see new blogs

--- java-czec/src/main/java/cz/jiripinkas/jba/controller/UserController.java
+++ java-czec/src/main/java/cz/jiripinkas/jba/controller/UserController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import cz.jiripinkas.jba.entity.Blog;
 import cz.jiripinkas.jba.entity.User;
+import cz.jiripinkas.jba.service.BlogService;
 import cz.jiripinkas.jba.service.UserService;
 
 @Controller
@@ -19,11 +21,19 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private BlogService blogService;
+	
+	
 	@ModelAttribute("user")
 	public User construct() {
 		return new User();
 	}
 
+	@ModelAttribute("blog")
+	public Blog constructBlog() {
+		return new Blog();
+	}
 	
 	@RequestMapping("/users")
 	public String users(Model model) {
@@ -58,4 +68,10 @@ public class UserController {
 		return "user-detail";
 	}
 	
+	@RequestMapping(value = "/account", method = RequestMethod.POST)
+	public String doAddBlog(@ModelAttribute("blog") Blog blog, Principal principal) {
+		String name = principal.getName();
+		blogService.save(blog, name);
+		return "redirect:/account.html";
+	}
 }
